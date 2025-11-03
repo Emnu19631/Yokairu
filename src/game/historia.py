92ed4011491d5d99e49d9576334ac4b730e06bfa
Emanuel C <emanuel19631@gmail.com>
2025-11-02 22:42:39 -0500

    Historia

--- src/game/historia.py
+++ src/game/historia.py
@@ -8,6 +8,9 @@ from core.render import mostrar_texto_tipeado_con_fondo_solido
 # ===============================
 
 BACKGROUND_JUEGO = "background_juego.jpg"
+BACKGROUND_VOLCAN = "background_volcan.png"
+BACKGROUND_NEGRO= "background_negro.jpg"
+BACKGROUND_ISLA="background_isla.png"
 SLIDE_5_01 = "slide5-01.jpg"
 SLIDE_5_02 = "slide5-02.jpg"
 SLIDE_5_03 = "slide5-03.jpg"
@@ -63,7 +66,7 @@ HISTORIA = [
             {"texto": "Al pueblo", "next": 4},
             {"texto": "A la isla", "next": None},
             {"texto": "Al prado", "next": None},
-            {"texto": "Al volcán", "next": None},
+            {"texto": "Al volcán", "next": 11},
         ],
     },
     {
@@ -164,5 +167,186 @@ HISTORIA = [
             "una chica que por algún motivo se me hacia conocida "
             "aunque probablemente solo era mi imaginacion)"
         ),
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_JUEGO,
+        "texto": (
+            "Decidí ir al volcán, se veía interesante aunque no parecía tener mucha vida. "
+            "pero igualmente decidí probar mi suerte, después de todo necesito ver que  "
+            "Eso fue hasta que alguien finalmente se me acercó: "
+            "puedo encontrar "
+        )
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_VOLCAN,
+        
+        "texto": (
+            "Sin embargo al llegar al volcán solo había destrucción y parecía estar "
+            "completamente abandonado pero creí ver algo extraño en una esquina, tal vez  "
+            "no era nada y solo era mi imaginacion, aun asi decidi acercarme pero al "
+            "hacerlo……"
+        ),
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_NEGRO,
+        
+        "texto": (
+            "Todo se torno negro y me parecio escuchar a alguien pero no lograba ver  "
+            "mucho, solo una silueta que intente seguir con cuidado para no caerme o"
+            "tropezarme con algo al avanzar"
+        ),
+        "imagen": "Meiro_exe_silueta.png",
+        "scalar": 0.4,
+        "pos_rel": (0.35, 0.15)
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_JUEGO,
+        "transicion_fondos": [
+            SLIDE_5_01, SLIDE_5_02, SLIDE_5_03,
+            SLIDE_5_04, SLIDE_5_05, SLIDE_5_06, SLIDE_5_07
+        ],
+        "texto": (
+            "Aquella figura dijo “tu no deberias estar aqui” y despues de eso apareci en "
+            "donde estaba al inicio pero había un pequeño detalle, el lugar, si bien era el "
+            "mismo se veía distinto"
+        ),
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_JUEGO,
+        "texto": (
+            "Ahora el volcán ya no estaba, era como si hubiera sido borrado  "
+            "completamente o algo parecido así que ahora solo podía ir al pueblo, prado"
+            "o la isla"
+        ),
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_JUEGO,
+        "texto": (
+            "Entonces, a dónde iré ahora?"
+        ),
+    },
+    {
+        "tipo": "eleccion",
+        "fondo": BACKGROUND_JUEGO,
+        "texto": "",
+        "layout": "2x2",
+        "opciones": [
+            {"texto": "Al pueblo", "next": 4},
+            {"texto": "A la isla", "next": 18},
+            {"texto": "Al prado", "next": None},
+        ],
+    },
+    { "tipo": "narracion",
+        "fondo": BACKGROUND_ISLA,
+        
+        "texto": (
+            "La isla se veía demasiado interesante al estar hecha de hielo además de que  "
+            "me pareció ver un oso polar por allí por lo que fui a explorar la misma  "
+            "tranquilamente, mientras caminaba alguien se me acercó aunque desconocía si "
+            "era hombre o mujer"
+        ),
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_ISLA,
+        
+        "texto": (
+            "Oh! Hola, soy Lapi Minato, un tiburón y rey del mar Diamonds Blue, es un  "
+            "placer conocerte mi estimada, por lo visto necesitas algo de ayuda, no es"
+            "así?"
+        ),
+        "imagen": "Lapi_a.png",
+        "scalar": 0.4,
+        "pos_rel": (0.35, 0.15)
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_ISLA,
+        
+        "texto": (
+            "Así que Lapi, interesante, suena como lápiz, es gracioso, y qué más dijo? rey  "
+            "del mar? entonces es un chico? bueno, yo no juzgo pero se ve algo femenino "
+            "aunque me gusta su estilo sinceramente, tal vez debería preguntarle algo sobre "
+            "este lugar"
+        ),
+        "imagen": "Lapi_b.png",
+        "scalar": 0.4,
+        "pos_rel": (0.35, 0.15)
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_ISLA,
+        
+        "texto": (
+            "Entonces, hay algo que te gustaría saber sobre este lugar?"
+        ),
+        "imagen": "Lapi_b.png",
+        "scalar": 0.4,
+        "pos_rel": (0.35, 0.15)
+    },
+    {
+        "tipo": "eleccion",
+        "fondo": BACKGROUND_ISLA,
+        
+        "texto": (
+            "Entonces, hay algo que te gustaría saber sobre este lugar?"
+        ),
+        "imagen": "Lapi_b.png",
+        "scalar": 0.4,
+        "pos_rel": (0.35, 0.15)
+    },
+    {
+        "tipo": "eleccion",
+        "fondo": BACKGROUND_ISLA,
+        "imagen": "Lapi_c.png",
+        "scalar": 0.4,
+        "pos_rel": (0.35, 0.15),
+        "opciones": [
+            {"texto": "Si eres un tiburón, por qué no luces como uno?", "next": 24},
+            {"texto": "Vives en el mar?", "next": 24},
+        ],
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_ISLA,
+        
+        "texto": (
+            "Ese es un secreto que no revelaré por ahora pero te prometo que algún"
+            "día lo sabrás"
+        ),
+        "imagen": "Lapi_d.png",
+        "scalar": 0.4,
+        "pos_rel": (0.35, 0.15)
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_ISLA,
+        
+        "texto": (
+            "Y hablando de secretos, hay algo que debo mostrarte y alguien que quiere presentarse"
+            "Mencionó para luego de eso tomar mi mano y llevarme con él a conocer a aquella persona"
+            "misteriosa"
+        ),
+        "imagen": "Lapi_d.png",
+        "scalar": 0.4,
+        "pos_rel": (0.35, 0.15)
+    },
+    {
+        "tipo": "narracion",
+        "fondo": BACKGROUND_ISLA,
+        
+        "texto": (
+            "Si, soy un tiburón y vivo en el mar pero ahí veces que me gusta estar en"
+            "la tierra para hacer ciertas cosas que no revelaré"
+        ),
+        "imagen": "Lapi_e.png",
+        "scalar": 0.4,
+        "pos_rel": (0.35, 0.15)
     },
 ]
